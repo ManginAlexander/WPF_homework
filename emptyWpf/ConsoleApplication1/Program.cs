9d5068ced919cd3df98a6337c1bafd255053a955
Mangin.Alexander <mywork.koder@gmail.com>
2012-10-18 16:32:08 +0600

    Задача 1
    Научилсчя вызывать wpf из консоли

---
+++ emptyWpf/ConsoleApplication1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows;
+
+namespace ConsoleApplication1
+{
+	class Program
+	{
+		public class AppCode : Application
+		{
+			[STAThread]
+			public static void Main()
+			{
+				var app = new AppCode();
+				var win = new Window();
+				app.Run(win);
+			}
+		}
+	}
+}
